10deb6d720be6e350a32b9b99289ddd4beb14f9b
lian-c <lian.chan@telus.com>
2022-11-28 19:09:23 -0800

    Created urls_new ejs file to have the ability to add additional shortlink urls, also created function that provides 6 digit string of num/letters to be used as the id

--- express_server.js
+++ express_server.js
@@ -7,7 +7,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = () => {
+  const alphaNumeric = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const result = [];
+  for (let i = 0; i < 6; i ++){
+    const index = Math.floor(Math.random() * (alphaNumeric.length))
+    result.push(alphaNumeric[index])
+  } return result.join("")
+};
+
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,9 +40,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => { //:id doesn't have to be id but req.params.XX has to match :XX and on the ejs file as well
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
   res.render("urls_show", templateVars);
-  // console.log(req.params)
+  // console.log(req.params)  
+});
 
- });
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
+
