2bccaa6a6401b2a1cd420417cd41b396eeee582c
lian-c <lian.chan@telus.com>
2022-11-28 16:53:02 -0800

    Created ejs file to view with the server file

--- express_server.js
+++ express_server.js
@@ -16,13 +16,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase}
+  res.render("urls_index", templateVars);
  });
